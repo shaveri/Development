0c3641f13149ddf16ab0e47a9608b9abb4ec2813
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-02 01:35:20 -0700

    feature1 new

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		System.out.println("Feature1 change");
+		System.out.println("Feature1 new change");
 		System.out.println("Feature 2");
     }
 
