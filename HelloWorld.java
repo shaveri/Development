51420729f754e5d3038457f362c38275d7d32a1e
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-20 09:28:48 -0700

    index on atm: 8532e74 atm 1.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World, atm 1.0");
-
-		//atm changes
-		String str = new Stirng ("atm");
+        System.out.println("Hello, World");
+		//core changes
+		String str = new Stirng ("core 3.0");
     }
 
 }
