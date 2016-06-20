f977278033aeec76ba84888a8f1c06c346e5cd6b
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-20 10:17:01 -0700

    atm 3.0 merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+<<<<<<< HEAD
 		//atm changes
 		String str = new Stirng ("atm 3.0");
+=======
+		//core changes
+		String str = new Stirng ("core 3.1");
+>>>>>>> core
     }
 
 }
