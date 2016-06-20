8532e74c53cc1dd17f3114bd347c4c4aa0e90389
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-20 09:24:56 -0700

    atm 1.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World, core 2.0");
-		
-
-		//core changes
-		Thread t = new Thread(new Runnable());
+        System.out.println("Hello, World, atm 1.0");
 
 		//atm changes
 		String str = new Stirng ("atm");
