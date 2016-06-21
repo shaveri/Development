05fe71d4d92ee7e388aad50e7fe3afe71ee6fd5a
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-21 01:30:09 -0700

    master 1.0

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         // Prints "Hello, Test" to the terminal window.
+		//master file
         System.out.println("Hello, Test");
     }
 
