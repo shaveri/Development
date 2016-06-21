ac91886d8bff4f0338bc4a23d7596659a60157cf
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-21 01:14:03 -0700

    core merged with atm 3.1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+		//core changes
+		String str = new Stirng ("core 4.1");
 		//atm changes
-		String str = new Stirng ("atm 3.1");
+		String str = new String ("atm 3.1");
     }
-
 }
