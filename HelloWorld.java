6a8fb0402977b34df04a554d083869f902ae6136
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-21 01:28:00 -0700

    master 1.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		//core changes
-		String str = new Stirng ("core 4.1");
-		//atm changes
-		String str = new String ("atm 3.1");
     }
 }
