38e3199e4822c5dbde958d0355a3f05eb0210639
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-24 00:08:57 -0700

    core 1.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		//atm changes
+		//core changes
+		private static Log log = LogFactory.getLog(HelloWorld.class);		
     }
 }
