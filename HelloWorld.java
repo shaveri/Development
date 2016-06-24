bac956267cb7407f7855e7a1ad519840b2ff2332
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-24 00:09:55 -0700

    atm 1.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+		//atm changes
+		private static SessionFactory defaultSessionFactory;
+		private static Configuration defaultHibernateConfiguration;
     }
 }
