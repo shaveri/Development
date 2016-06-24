d3265ad67d0059318c360b8df029a18c93590b5a
Saleem Haveri <shaveri@accuratebackground.com>
2016-06-24 01:53:07 -0700

    core merged with atm

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+<<<<<<< HEAD
 		//atm changes
 		private static SessionFactory defaultSessionFactory;
 		private static Configuration defaultHibernateConfiguration;
+=======
+		//core changes
+		DOMConfigurator.configure( pRoot + "/abc-bo/test/data/log4j.xml");
+		String  hbConfig = pRoot + "/abc-bo/test/data/hibernate.cfg.xml";    // configure HB
+>>>>>>> core
     }
 }
